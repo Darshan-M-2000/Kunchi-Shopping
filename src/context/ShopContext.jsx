ef3d72c3907403faa769fa0ce31926649fc9ad0e
Darshan-M-2000 <darshan002000@gmail.com>
2023-10-20 14:05:28 +0530

    loginSignup jsx and css added

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,16 +1,16 @@
-// import React,{createContext} from 'react';
-// import all_product from './components/assets/all_product';
+import React,{createContext} from 'react';
+import all_product from '../components/assets/all_product';
 
-// export const ShopContext = createContext(null);
-
-// const ShopContextProvider=(props)=>{
-//     const contextValue={all_product};
+export const ShopContext = createContext(null);
 
 
-//     return(
-//         <ShopContext.Provider value={contextValue}>
-//             {props.children}
-//         </ShopContext.Provider>
-//     )
-// }
-// export default ShopContextProvider;
+const ShopContextProvider = (props) => {
+    const contextValue = {all_product};
+  return (
+    <ShopContext.Provider value={contextValue}>
+        {props.children}
+    </ShopContext.Provider>
+  )
+}
+
+export default ShopContextProvider ;
--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Css/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsingup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsign-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder="Email Address"  />
+          <input type="password" placeholder='password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account?<span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+          <p>By continuing, i agree to the subsrcibe to KUNCHI VIBE'S YouTue channel</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
