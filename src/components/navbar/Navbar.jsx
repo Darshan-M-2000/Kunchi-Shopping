a994e6343b3456af0dac6b9738c486fc972db50b
Darshan-M-2000 <darshan002000@gmail.com>
2023-10-23 10:01:28 +0530

    Shop spelling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt='logo'/>
-            <p>KUNCHI-SHOPPING</p>
+            <p>KUNCHI-SHOP</p>
         </div>
         <ul className="nav-menu">
             <li onClick={()=>{setMenu("shop")}}><Link to='/' className='underline' >Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
